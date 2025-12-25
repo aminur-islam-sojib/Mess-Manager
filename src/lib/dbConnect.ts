35a2fdd6d7acb640982b3a82253878633c7465ad
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-12-25 10:42:07 +0600

    Connect Mongo DB

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGO_URI;
+const dname = process.env.DB_NAME;
+
+// here code changes
+if (!uri) {
+  throw new Error("❌ Please add MONGO_URI to .env.local");
+}
+
+// here code changes
+if (!dname) {
+  throw new Error("❌ Please add DB_NAME to .env.local");
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = async (cname: string) => {
+  return client.db(dname).collection(cname);
+};
